d62eda47e7600c14fc04cfe1bed06ca34467cf1f
JoaoPresto <jguedespresto@gmail.com>
2020-10-21 16:21:07 -0300

    Ajuste

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const bodyParser = require('body-parser');
 const routerParking = require('./module/parking/router');
 const routerWifi = require('./module/wifi/router');
 const cors = require('cors');
-const port = process.env.APP_URL || 3000;
+const port = process.env.APP_URL || 5000;
+const hostname = 'localhost'
 
 app.use(cors());
 app.use(bodyParser.json())
 app.use('/api/parking', routerParking)
 app.use('/api/wifi', routerWifi)
 
-app.listen(port, () => {
-    console.log(`Api initilize success in port ${port}`)
-})
+// app.listen(port, hostname, () => {
+//     console.log(`Api initilize success in port ${port}`)
+// })
