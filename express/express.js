dc1e80817b837f095eaf8582c7dafe75846813e4
JoaoPresto <jguedespresto@gmail.com>
2020-10-21 15:16:07 -0300

    Add file Heroku

--- express/express.js
+++ express/express.js
@@ -1,8 +1,9 @@
+require("dotenv").config()
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
-mongoose.connect('mongodb+srv://Wifi:Wifi@2020@cluster0.l85oj.mongodb.net/reactEstacionamento?retryWrites=true&w=majority',
+mongoose.connect(process.env.MONGO_URL,
     {
         useCreateIndex: true,
         useNewUrlParser: true,
