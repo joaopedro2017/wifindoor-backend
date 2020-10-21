57e65e741cfbe8629bc3cd56aa8aef82c3a7e05d
JoaoPresto <jguedespresto@gmail.com>
2020-10-21 16:37:36 -0300

    Config port

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const bodyParser = require('body-parser');
 const routerParking = require('./module/parking/router');
 const routerWifi = require('./module/wifi/router');
 const cors = require('cors');
-const port = process.env.APP_URL || 5000;
-const hostname = 'localhost'
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json())
 app.use('/api/parking', routerParking)
 app.use('/api/wifi', routerWifi)
 
-// app.listen(port, hostname, () => {
-//     console.log(`Api initilize success in port ${port}`)
-// })
+app.listen(port, () => {
+    console.log(`Api initilize success in port ${port}`)
+})
