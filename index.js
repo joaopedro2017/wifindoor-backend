8455c5cfad0d5dd142f6422092cb8fe23fdf998e
JoaoPresto <jguedespresto@gmail.com>
2020-10-21 14:44:37 -0300

    Add cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const app = require('./express/express')
-const bodyParser = require('body-parser')
-const routerParking = require('./module/parking/router')
-const routerWifi = require('./module/wifi/router')
-const port = process.env.port || 5000
+const app = require('./express/express');
+const bodyParser = require('body-parser');
+const routerParking = require('./module/parking/router');
+const routerWifi = require('./module/wifi/router');
+const cors = require('cors');
+const port = process.env.port || 5000;
 
+app.use(cors());
 app.use(bodyParser.json())
-
 app.use('/api/parking', routerParking)
 app.use('/api/wifi', routerWifi)
 
